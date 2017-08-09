895dc3ec4e1f16e528b175aa2da7f5b3d1810e5f
fractalfox01 <flgtom@aol.com>
2017-08-09 12:03:22 -0700

    fixed spelling errors

--- src/BillsBurgerShop.java
+++ src/BillsBurgerShop.java
@@ -1,8 +1,8 @@
 /*
 Author: FractalFox
 
-Program Requirments:
-        The purpose of the application is to help a ficticious company calles Bill's Burgers to manage
+Program Requirements:
+        The purpose of the application is to help a fictitious company called Bill's Burgers to manage
         their process of selling hamburgers.
         Our application will help Bill to select types of burgers, some of the additional items (additions) to
         be added to the burgers and pricing.
@@ -11,16 +11,16 @@ Program Requirments:
         The basic hamburger should hae the following items.
         bread roll type, meat and up to 4 additional toppings (things like lettuce, tomato, carrot, etc) that
         the customer can select to be added to the burger. Each one of these items gets charged an additional
-        price so you need some way to track how many items got added and to calculae the price (for the base
+        price so you need some way to track how many items got added and to calculate the price (for the base
         burger and all the additions).
-        This burger has a base price and the additions are all seperately priced.
+        This burger has a base price and the additions are all separately priced.
         Create a Hamburger class to deal with all the above.
         The constructor should only include the roll type, meat, and price.
-        Also create two extra verieties of Hamburgers (classes) to cater for
+        Also create two extra varieties of Hamburgers (classes) to cater for
         a) Healthy burger can have a total of 6 items (additions) in total.
-        hint: you have to find a way to automatically add these new additions at the time the deluze burger
+        hint: you have to find a way to automatically add these new additions at the time the delude burger
         object is created, and then prevent other additions being made.
-        All 3 classes should have a method that can be called anytime to show th ebase price of the hamburger
+        All 3 classes should have a method that can be called anytime to show the base price of the hamburger
         plus all additionals, each showing the addition name, and addition price, and a grand total fot the
         burger.
         For the two additional classes this may require you to be looking at the base class for pricing and then
