327668062ce2b86001034cebfde8d0894bf26b78
fractalfox01 <flgtom@aol.com>
2017-08-09 09:56:29 -0700

    added project scope in BillsBurgerShop

--- src/BillsBurgerShop.java
+++ src/BillsBurgerShop.java
@@ -1,6 +1,40 @@
+/*
+Author: FractalFox
+
+Program Requirments:
+        The purpose of the application is to help a ficticious company calles Bill's Burgers to manage
+        their process of selling hamburgers.
+        Our application will help Bill to select types of burgers, some of the additional items (additions) to
+        be added to the burgers and pricing.
+        We want to create a base hamburger, but also two other types of hamburgers that are popular ones in
+        Bill's store.
+        The basic hamburger should hae the following items.
+        bread roll type, meat and up to 4 additional toppings (things like lettuce, tomato, carrot, etc) that
+        the customer can select to be added to the burger. Each one of these items gets charged an additional
+        price so you need some way to track how many items got added and to calculae the price (for the base
+        burger and all the additions).
+        This burger has a base price and the additions are all seperately priced.
+        Create a Hamburger class to deal with all the above.
+        The constructor should only include the roll type, meat, and price.
+        Also create two extra verieties of Hamburgers (classes) to cater for
+        a) Healthy burger can have a total of 6 items (additions) in total.
+        hint: you have to find a way to automatically add these new additions at the time the deluze burger
+        object is created, and then prevent other additions being made.
+        All 3 classes should have a method that can be called anytime to show th ebase price of the hamburger
+        plus all additionals, each showing the addition name, and addition price, and a grand total fot the
+        burger.
+        For the two additional classes this may require you to be looking at the base class for pricing and then
+        adding totals onto that.
+         */
 public class BillsBurgerShop {
     public static void main(String[] args){
-        System.out.println("Bills' Burger Shop:\nProduction Management SoftWare");
+
+        System.out.println("------------------------------\n\t Bills' Burger Shop\nProduction Management SoftWare\n------------------------------\n\n");
 
     }
 }
+
+
+
+
+
